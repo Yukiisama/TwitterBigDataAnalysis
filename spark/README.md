aeef32b10be647e85971c874be1d2fdeda9b4493
Alexis Perignon <aperigno@emi.u-bordeaux.fr>
2020-12-25 17:43:01 +0100

    Add basic spark

---
+++ spark/README.md
@@ -0,0 +1,7 @@
+# how to launch
+
+mvn package
+cd spark_maven
+spark-submit target/TPSpark-0.0.1.jar
+
+maybe put others options on spark submit cmd for hbase etc...
---
+++ spark/spark_maven/src/main/java/bigdata/TPSpark.java
@@ -0,0 +1,16 @@
+package bigdata;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class TPSpark {
+
+	public static void main(String[] args) {
+		
+		SparkConf conf = new SparkConf().setAppName("TP Spark");
+		JavaSparkContext context = new JavaSparkContext(conf);
+		System.out.println("coucou");
+		
+	}
+	
+}
