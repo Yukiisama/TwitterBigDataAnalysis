c6b96441b77f17bd9383a4fc79ba6277364d4ed2
Jordane Masson <jmasson@emi.u-bordeaux.fr>
2020-12-27 16:26:53 +0100

    First work on Users information retrieval.

--- spark/spark_maven/src/main/java/bigdata/file/json/JsonUtils.java
+++ spark/spark_maven/src/main/java/bigdata/file/json/JsonUtils.java
@@ -2,16 +2,17 @@ package bigdata.file.json;
 
 
 
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
+import scala.Tuple2;
 
 public class JsonUtils {
     public static Iterator<String> withoutReflexivityAndWholeJson(String line) {
@@ -29,5 +30,38 @@ public class JsonUtils {
 			}
 		}
 		return hashs.iterator();
+    }
+    
+
+    public static Tuple2 <String, HashSet<String>> withoutReflexivityAndWholeJsonUsers(String requested_user_id, String line) {
+		Tuple2 <String, HashSet<String>> res = new Tuple2<String, HashSet<String>>(requested_user_id, new HashSet<String>());
+
+		HashSet<String> hashs = new HashSet<>();
+		JsonElement json = new JsonParser().parse(line);
+		JsonObject jsonObj = json.getAsJsonObject();
+		JsonElement users = jsonObj.get("user");
+		JsonElement entities = jsonObj.get("entities");
+		if (entities != null && entities.isJsonObject()) {
+            JsonElement hashtags = (entities.getAsJsonObject()).get("hashtags");
+			String user_id = (users.getAsJsonObject().get("name")).getAsString();
+			
+			if(!user_id.equals(requested_user_id)) {
+				return res;
+			}
+
+            
+            
+			if (hashtags != null) {
+				for (JsonElement hash : hashtags.getAsJsonArray()) {
+					hashs.add(hash.getAsJsonObject().get("text").getAsString());
+                }
+            }
+            
+			res = new Tuple2<String, HashSet<String>>(jsonObj.get("id").getAsString(), hashs);
+			
+
+			System.out.println("Found a value:" + res);
+		}
+		return res;
 	}
 }
